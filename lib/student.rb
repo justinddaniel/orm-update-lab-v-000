2246991f298d275e48b38a253018e809d8ba3887
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 20:37:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,12 +6,21 @@ class Student
   attr_reader :id
 
   def initialize(id = nil, name, grade)
-    @id = id
-    @name = name
+    @id = id 
+    @name = name 
     @grade = grade
   end
 
   def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+
+DB[:conn].execute(sql)
   end
 
   def self.drop_table
@@ -31,6 +40,6 @@ class Student
 
   def update
   end
-
+  
 
 end
