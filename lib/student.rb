e35ff1a08b5000b61e8fde9d8f02ff1ccc4c6b4f
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 20:34:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,35 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name, :grade
+  attr_reader :id
 
+  def initialize(id = nil, name, grade)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end
+
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create(name, grade)
+  end
+
+  def self.new_from_db(row)
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def update
+  end
+  
 
 end
